8f6de5164ffbd151a1600021a3294198c6be5ba9
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-23 17:03:58 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendCat() {
+    return cats.push('Ralph')
+} 
+
+function destructivelyAppendCat() {
+    return cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat() {
+    let newCats = [...cats.slice(), "Broom"]
+    return newCats
+}
+
+function prependCat() {
+    let newCats = [ "Arnold", ...cats.slice()]
+    return newCats
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+function removelastCat() {
+    let newCats = [...cats.slice(0, -1)]
+    return newCats
+}
